a3d5c5d8eae8d84d9a1d0a029a04f7206ea17825
harbingerofme <harbingerofme@gmail.com>
2019-10-09 09:46:49 +0200

    Create setup.bat

---
+++ libs/setup.bat
@@ -0,0 +1,7 @@
+git clone --single-branch --branch bep150 https://github.com/Paddywaan/RoR2Libs
+del RoR2Libs\.gitattributes
+del RoR2Libs\.gitignore
+del RoR2Libs\RoR2Libs.sln
+rmdir /S /Q RoR2Libs\.git
+copy RoR2Libs\libs\*.dll .\
+rmdir RoR2Libs
