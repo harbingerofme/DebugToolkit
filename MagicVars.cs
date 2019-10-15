1d148c120a9258fbd9e34d763c588f6a991d12b1
harbingerofme <harbingerofme@gmail.com>
2019-10-15 19:22:04 +0200

    Now alphabetical

--- MagicVars.cs
+++ MagicVars.cs
@@ -6,21 +6,21 @@ namespace RoR2Cheats
 {
     public static class MagicVars
     {
-        public const string 
-            SPAWN_ERROR = "Could not spawn: ",
-            SPAWN_ATTEMPT = "Attempting to spawn: ",
-            PLAYER_NOTFOUND = "Specified player does not exist",
-            PLAYER_DEADRESPAWN = "Player is dead and cannot respawn.",
-            OBJECT_NOTFOUND = "The requested object could not be found: ",
+        public const string
             GIVEEQUIP_ARGS = "Requires 1 argument: give_equip {localised_object_name} {playername}",
             GIVEITEM_ARGS = "Requires 1 argument: give_item {localised_object_name} {count} {playername}",
+            INTEGER_EXPECTED = "Argument must be a whole number.",
+            OBJECT_NOTFOUND = "The requested object could not be found: ",
+            OBSOLETEWARNING = "This command has become obsolete and will be removed in the next version. ",
+            PLAYER_DEADRESPAWN = "Player is dead and cannot respawn.",
+            PLAYER_NOTFOUND = "Specified player does not exist",
+            PORTAL_NOTFOUND = "The specified portal could not be found: add_portal {blue|celestial|gold}",
+            RUNSETSTAGESCLEARED_HELP = "Sets the amount of stages cleared. This does not change the current stage.",
+            SPAWN_ATTEMPT = "Attempting to spawn: ",
+            SPAWN_ERROR = "Could not spawn: ",
             SPAWNAS_ARGS = "Requires 1 argument: spawn_as {localised_objectname} {playername}",
             SPAWNBODY_ARGS = "Requires 1 argument: spawn_body {localised_objectname}",
-            TEAM_ARGS = "Requires 1 argument: change_team {TeamIndex} {playername}",
-            INTEGER_EXPECTED = "Argument must be a whole number.",
-            RUNSETSTAGESCLEARED_HELP = "Sets the amount of stages cleared. This does not change the current stage.",
-            OBSOLETEWARNING = "This command has become obsolete and will be removed in the next version. ",
-            PORTAL_NOTFOUND = "The specified portal could not be found: add_portal {blue|celestial|gold}"
+            TEAM_ARGS = "Requires 1 argument: change_team {TeamIndex} {playername}"
             ;
     }
 }
