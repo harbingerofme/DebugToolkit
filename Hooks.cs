349574f52b1f4c874c41c2d7618ce029191dac08
harbingerofme <harbingerofme@gmail.com>
2019-10-15 10:51:01 +0200

    freeTheConvars
    fixes #3

--- Hooks.cs
+++ Hooks.cs
@@ -3,17 +3,17 @@ using System;
 using UnityEngine;
 using RoR2;
 using R2API.Utils;
-using Console = RoR2.Console;
 
 namespace RoR2Cheats
 {
     public class Hooks
     {
-        
+        private const ConVarFlags AllFlagsNoCheat = ConVarFlags.None | ConVarFlags.Archive | ConVarFlags.Engine | ConVarFlags.ExecuteOnServer | ConVarFlags.SenderMustBeServer;
         public static void InitializeHooks()
         {
             UnlockConsole();
             ConCommandHooks();
+            FreeTheConvars();
 
             SeedHooks();
 
@@ -27,11 +27,33 @@ namespace RoR2Cheats
             //IL.RoR2.Networking.GameNetworkManager.cctor += GameNetworkManager_cctor;
         }
 
+        private static void FreeTheConvars()
+        {
+            void removeCheatFlag (RoR2.ConVar.BaseConVar cv)
+            {
+                cv.flags &= AllFlagsNoCheat;
+            }
+
+            On.RoR2.Console.InitConVars += (orig, self) =>
+            {
+                orig(self);
+                removeCheatFlag(self.FindConVar("sv_time_transmit_interval"));
+                removeCheatFlag(self.FindConVar("run_scene_override"));
+                removeCheatFlag(self.FindConVar("stage1_pod"));
+                self.FindConVar("timescale").helpText += " Use time_scale instead!";
+                self.FindConVar("director_combat_disable").helpText += " Use no_enemies instead!";
+                self.FindConVar("timestep").helpText += " Let the ror2cheats team know if you need this convar.";
+                self.FindConVar("cmotor_safe_collision_step_threshold").helpText += " Let the ror2cheats team know if you need this convar.";
+                self.FindConVar("cheats").helpText += " But you already have the RoR2Cheats mod installed...";
+            };
+
+            
+        }
+
         private static void UnlockConsole()
         {
             IL.RoR2.Console.Awake += (ILContext il) =>
             {
-                const ConVarFlags flags = ConVarFlags.None | ConVarFlags.Archive | ConVarFlags.Engine | ConVarFlags.ExecuteOnServer | ConVarFlags.SenderMustBeServer;
                 ILCursor c = new ILCursor(il);
                 c.GotoNext(
                     MoveType.After,
@@ -40,7 +62,7 @@ namespace RoR2Cheats
                 c.EmitDelegate<Func<ConCommandAttribute, ConCommandAttribute>>(
                     (conAttr) =>
                     {
-                        conAttr.flags &= flags;
+                        conAttr.flags &= AllFlagsNoCheat;
                         if (conAttr.commandName == "run_set_stages_cleared")
                         {
                             conAttr.helpText = MagicVars.RUNSETSTAGESCLEARED_HELP;
