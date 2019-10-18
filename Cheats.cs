a2ed50ddd8cafef00e11eb89194579a0c4cad417
harbingerofme <harbingerofme@gmail.com>
2019-10-18 15:24:59 +0200

    versioning

--- Cheats.cs
+++ Cheats.cs
@@ -16,7 +16,7 @@ namespace RoR2Cheats
     [BepInPlugin("com.harbingerofme." + modname, modname, modver)]
     public class RoR2Cheats : BaseUnityPlugin
     {
-        public const string modname = "RoR2Cheats", modver = "3.0.1";
+        public const string modname = "RoR2Cheats", modver = "3.1.0";
         public static bool noEnemies = false;
         public static ulong seed =0;
         public static float TickIntervalMulti = 1f;
