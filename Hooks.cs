72981fdc00cb02379764073c797dcf8c357c1b66
Harbingerofme <harbingerofme@gmail.com>
2019-11-01 12:58:46 +0100

    Is what I get for not properly testing

--- Hooks.cs
+++ Hooks.cs
@@ -66,8 +66,8 @@ namespace RoR2Cheats
             self.FindConVar("timestep").helpText += " Let the ror2cheats team know if you need this convar.";
             self.FindConVar("cmotor_safe_collision_step_threshold").helpText += " Let the ror2cheats team know if you need this convar.";
             self.FindConVar("cheats").helpText += " But you already have the RoR2Cheats mod installed...";
-            BaseConVar mmConvar = self.FindConVar("max_messages");
-            if(mmConvar.GetString() == mmConvar.defaultValue)
+            IntConVar mmConvar = (IntConVar) self.FindConVar("max_messages");
+            if(mmConvar.value == 25)
             {
                 mmConvar.SetString("100");
             }
