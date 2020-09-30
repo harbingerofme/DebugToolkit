19eee66f6294f0c093c54d2bf5c2c0eafd92d625
Death <xiaoxiao921@hotmail.fr>
2020-09-30 22:08:04 +0200

    Add Assembly Attribute that tells the JIT to not check for correctness of access modifiers in our assembly

---
+++ Code/IgnoreAccessModifiers.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Permissions;
+
+// SecurityPermision set to minimum set to true
+// for skipping access modifiers check from the mono JIT
+// The same attribute is added to the assembly when ticking
+// Unsafe Code in the Project settings
+// This is done here to allow an explanation of the trick and
+// not in an outside source you could potentially miss.
+
+#pragma warning disable CS0618 // Type or member is obsolete
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+#pragma warning restore CS0618 // Type or member is obsolete
