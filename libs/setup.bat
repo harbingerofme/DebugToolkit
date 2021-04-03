6f814d9bd532b957bad13e91728a56015184ad68
HarbingerOfMe <harbingerofme@gmail.com>
2021-04-03 12:08:06 +0200

    clean up build process

--- libs/setup.bat
+++ libs/setup.bat
@@ -1,6 +1,8 @@
 git clone https://github.com/harbingerofme/RoR2Libs.git
-xcopy /E /I RoR2Libs .\
+rmdir /S /Q RoR2Libs\scripts\
+del RoR2Libs\Managed\Assembly-CSharp.dll
+cp -r ./RoR2Libs ./
 rmdir /S /Q RoR2Libs
-rmdir /S /Q scripts
 del README.MD
 del .gitignore
+
