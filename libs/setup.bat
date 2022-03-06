8a296febd5a1cf9d1a4d0f228042e0ab8dd017bd
HarbingerOfMe <harbingerofme@gmail.com>
2022-03-06 13:13:37 +0100

    project build things

--- libs/setup.bat
+++
@@ -1,8 +0,0 @@
-git clone https://github.com/harbingerofme/RoR2Libs.git
-rmdir /S /Q RoR2Libs\scripts\
-del RoR2Libs\Managed\Assembly-CSharp.dll
-cp -r ./RoR2Libs ./
-rmdir /S /Q RoR2Libs
-del README.MD
-del .gitignore
-
