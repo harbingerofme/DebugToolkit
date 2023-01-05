57478e67c908134f8aea0117307515e5c0010d7b
HarbingerOfMe <harbingerofme@gmail.com>
2023-01-05 19:40:25 +0100

    Version bumping

--- Code/DebugToolkit.cs
+++ Code/DebugToolkit.cs
@@ -17,7 +17,7 @@ namespace DebugToolkit
     [BepInPlugin(GUID, modname, modver)]
     public class DebugToolkit : BaseUnityPlugin
     {
-        public const string modname = "DebugToolkit", modver = "3.7.1";
+        public const string modname = "DebugToolkit", modver = "3.7.2";
         public const string GUID = "iHarbHD." + modname;
 
         internal static ConfigFile Configuration;
