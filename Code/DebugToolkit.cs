1f55a7b2c3534f0da526e26f19a36c976a37525e
Quentin E. / iDeath <xiaoxiao921@hotmail.fr>
2023-05-08 00:34:58 +0200

    bump ver

--- Code/DebugToolkit.cs
+++ Code/DebugToolkit.cs
@@ -17,7 +17,7 @@ namespace DebugToolkit
     [BepInPlugin(GUID, modname, modver)]
     public class DebugToolkit : BaseUnityPlugin
     {
-        public const string modname = "DebugToolkit", modver = "3.8.0";
+        public const string modname = "DebugToolkit", modver = "3.8.1";
         public const string GUID = "iHarbHD." + modname;
 
         internal static ConfigFile Configuration;
