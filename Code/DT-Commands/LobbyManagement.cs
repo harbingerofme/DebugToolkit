1f67e29ba399313c8773fe17e3b98cf57bd0cea8
SChinchi <57867641+SChinchi@users.noreply.github.com>
2024-01-11 17:15:21 +0000

    refactor kin/ban client parsing

--- Code/DT-Commands/LobbyManagement.cs
+++ Code/DT-Commands/LobbyManagement.cs
@@ -19,47 +19,9 @@ namespace DebugToolkit.Commands
                 Log.MessageNetworked(Lang.INSUFFICIENT_ARGS + Lang.KICK_ARGS, args, LogLevel.Error);
                 return;
             }
-            NetworkUser nu = Util.GetNetUserFromString(args.userArgs);
-            if (nu == null)
-            {
-                Log.MessageNetworked(Lang.PLAYER_NOTFOUND, args, LogLevel.Error);
-                return;
-            }
-
-            // Check if we can kick targeted user.
-            if (!Application.isBatchMode)
-            {
-                foreach (var serverLocalUsers in NetworkUser.readOnlyLocalPlayersList)
-                {
-                    if (serverLocalUsers == nu)
-                    {
-                        Log.MessageNetworked("Specified user is hosting.", args, LogLevel.Error);
-                        return;
-                    }
-                }
-            }
-            else if (PermissionSystem.IsEnabled.Value)
-            {
-                if (!PermissionSystem.HasMorePerm(args.sender, nu, args))
-                {
-                    Log.MessageNetworked("The target has a higher permission level that you.", args, LogLevel.Error);
-                    return;
-                }
-            }
-
-            NetworkConnection client = null;
-            foreach (var connection in NetworkServer.connections)
-            {
-                if (nu.connectionToClient == connection)
-                {
-                    client = connection;
-                    break;
-                }
-            }
-
+            var client = GetClientFromArgs(args);
             if (client == null)
             {
-                Log.MessageNetworked("Error trying to find the associated connection with the user", args, LogLevel.Error);
                 return;
             }
             var reason = new NetworkManagerSystem.SimpleLocalizedKickReason("KICK_REASON_KICK");
@@ -73,50 +35,12 @@ namespace DebugToolkit.Commands
         {
             if (args.Count == 0)
             {
-                Log.MessageNetworked(Lang.BAN_ARGS, args, LogLevel.Error);
+                Log.MessageNetworked(Lang.INSUFFICIENT_ARGS + Lang.BAN_ARGS, args, LogLevel.Error);
                 return;
             }
-            NetworkUser nu = Util.GetNetUserFromString(args.userArgs);
-            if (nu == null)
+            var client = GetClientFromArgs(args);
+            if (client == null)
             {
-                Log.MessageNetworked(Lang.PLAYER_NOTFOUND, args, LogLevel.Error);
-                return;
-            }
-
-            // Check if we can kick targeted user.
-            if (!Application.isBatchMode)
-            {
-                foreach (var serverLocalUsers in NetworkUser.readOnlyLocalPlayersList)
-                {
-                    if (serverLocalUsers == nu)
-                    {
-                        Log.MessageNetworked("Specified user is hosting.", args, LogLevel.Error);
-                        return;
-                    }
-                }
-            }
-            else if (PermissionSystem.IsEnabled.Value)
-            {
-                if (!PermissionSystem.HasMorePerm(args.sender, nu, args))
-                {
-                    Log.MessageNetworked("The target has a higher permission level that you.", args, LogLevel.Error);
-                    return;
-                }
-            }
-
-            NetworkConnection client = null;
-            foreach (var connection in NetworkServer.connections)
-            {
-                if (nu.connectionToClient == connection)
-                {
-                    client = connection;
-                    break;
-                }
-            }
-
-            if (client != null)
-            {
-                Log.MessageNetworked("Error trying to find the associated connection with the user", args, LogLevel.Error);
                 return;
             }
             NetworkManagerSystem.singleton.ServerBanClient(client);
@@ -145,6 +69,55 @@ namespace DebugToolkit.Commands
 
             master.TrueKill();
             Log.MessageNetworked(master.name + " was killed by server.", args);
+        }
+
+        private static NetworkConnection GetClientFromArgs(ConCommandArgs args)
+        {
+            NetworkUser nu = Util.GetNetUserFromString(args.userArgs);
+            if (nu == null)
+            {
+                Log.MessageNetworked(Lang.PLAYER_NOTFOUND, args, LogLevel.Error);
+                return null;
+            }
+
+            // Check if we can kick targeted user.
+            if (!Application.isBatchMode)
+            {
+                foreach (var serverLocalUsers in NetworkUser.readOnlyLocalPlayersList)
+                {
+                    if (serverLocalUsers == nu)
+                    {
+                        Log.MessageNetworked("Specified user is hosting.", args, LogLevel.Error);
+                        return null;
+                    }
+                }
+            }
+            else if (PermissionSystem.IsEnabled.Value)
+            {
+                if (!PermissionSystem.HasMorePerm(args.sender, nu, args))
+                {
+                    Log.MessageNetworked("The target has a higher permission level that you.", args, LogLevel.Error);
+                    return null;
+                }
+            }
+
+            NetworkConnection client = null;
+            foreach (var connection in NetworkServer.connections)
+            {
+                if (nu.connectionToClient == connection)
+                {
+                    client = connection;
+                    break;
+                }
+            }
+
+            if (client == null)
+            {
+                Log.MessageNetworked("Error trying to find the associated connection with the user", args, LogLevel.Error);
+                return null;
+            }
+
+            return client;
         }
     }
 }
