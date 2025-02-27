c7a8ffd52fcfb5baba94418e8db497037591fb9e
SChinchi <57867641+SChinchi@users.noreply.github.com>
2025-02-27 15:46:41 +0000

    Fix spawn_ai NRE when explicitly using no elite

--- Code/DT-Commands/Spawners.cs
+++ Code/DT-Commands/Spawners.cs
@@ -204,7 +204,7 @@ namespace DebugToolkit.Commands
                     return;
                 }
                 eliteDef = EliteCatalog.GetEliteDef(eliteIndex);
-                if (eliteDef.eliteEquipmentDef && Run.instance.IsEquipmentExpansionLocked(eliteDef.eliteEquipmentDef.equipmentIndex))
+                if (eliteDef && eliteDef.eliteEquipmentDef && Run.instance.IsEquipmentExpansionLocked(eliteDef.eliteEquipmentDef.equipmentIndex))
                 {
                     var expansion = Util.GetExpansion(eliteDef.eliteEquipmentDef.requiredExpansion);
                     Log.MessageNetworked(string.Format(Lang.EXPANSION_LOCKED, "elite equipment", expansion), args, LogLevel.WarningClientOnly);
